6c37d2a8a23b49d2199c0ccc97b8f8172caca518
damirh <damir.huselja@in2.ba>
2023-02-20 21:27:31 +0100

    started menu development

--- src/main/java/com/saulpos/javafxcrudgenerator/HelloApplication.java
+++ src/main/java/com/saulpos/javafxcrudgenerator/HelloApplication.java
@@ -16,6 +16,19 @@ import java.net.URISyntaxException;
 import java.util.List;
 
 public class HelloApplication extends Application {
+
+    // todo: after developing menu concept move somewhere else, maybe to views or own folder menus
+    class MenuItem {
+        private String id;
+        private String name;
+        private String parentId;
+        public MenuItem(String id, String name, String parentId) {
+            this.id = id;
+            this.name = name;
+            this.parentId = parentId;
+        }
+    }
+
     @Override
     public void start(Stage stage) throws IOException, PropertyVetoException, URISyntaxException, ClassNotFoundException {
         Form form = Form.of(
@@ -61,8 +74,22 @@ public class HelloApplication extends Application {
             System.out.println(pr.getId() + " " + pr.getArea() + " "  + pr.getBarcode());
         }
 
+        // todo: after finishing initial concept for menu, move it from below
+        MenuItem[] menuMock;
+        menuMock = new MenuItem[10];
+        menuMock[0] = new MenuItem("0", "Main", null);
+        menuMock[1] = new MenuItem("1", "Product", "0");
+        menuMock[2] = new MenuItem("2", "Discount", "0");
+        menuMock[3] = new MenuItem("3", "Price", "0");
+        menuMock[4] = new MenuItem("4", "Stoager", "0");
+        menuMock[5] = new MenuItem("5", "Unit", "0");
+        menuMock[6] = new MenuItem("6", "Cashier", "0");
+        menuMock[7] = new MenuItem("7", "Administration", null);
+        menuMock[8] = new MenuItem("8", "Profile", "7");
+        menuMock[9] = new MenuItem("9", "User", "7");
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 640, 640);
 
         HelloController controller = fxmlLoader.<HelloController>getController();
         controller.mainVBox.getChildren().add(
