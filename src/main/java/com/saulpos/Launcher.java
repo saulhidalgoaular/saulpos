5c264ea5250221999a4a975d3dd2e088ff2be95d
Damir Huselja <damirh@in2.si>
2023-04-19 21:22:15 +0200

    Creating a autosleep feature for the application

--- src/main/java/com/saulpos/Launcher.java
+++ src/main/java/com/saulpos/Launcher.java
@@ -7,9 +7,16 @@ import com.saulpos.view.LoginView;
 import com.saulpos.view.ParentPane;
 import com.saulpos.view.Utils;
 import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Launcher extends Application {
 
@@ -26,6 +33,43 @@ public class Launcher extends Application {
         stage.setScene(new Scene(parentPane));
         stage.setMaximized(true);
         stage.show();
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.print("Timer Task fired!!!");
+            }
+        }, 10*1000);
+
+        stage.addEventHandler(KeyEvent.ANY, new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                timer.cancel();
+                Timer timer = new Timer();
+                timer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        System.out.print("Timer Task fired!!!");
+                    }
+                }, 10*1000);
+            }
+        });
+
+        stage.addEventHandler(MouseEvent.ANY, new EventHandler<Event>() {
+            @Override
+            public void handle(Event event) {
+                timer.cancel();
+                Timer timer = new Timer();
+                timer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        System.out.print("Timer Task fired!!!");
+                    }
+                }, 1*1000);
+            }
+        });
+
 
         Utils.goForward(new Utils.ViewDef("/login.fxml", loginPresenter), parentPane);
     }
--- src/main/java/com/saulpos/model/menu/action/LogoutMenuAction.java
+++ src/main/java/com/saulpos/model/menu/action/LogoutMenuAction.java
@@ -1,11 +1,28 @@
 package com.saulpos.model.menu.action;
 
+import com.saulpos.model.LoginModel;
+import com.saulpos.presenter.LoginPresenter;
+import com.saulpos.view.LoginView;
+import com.saulpos.view.ParentPane;
+import com.saulpos.view.Utils;
+import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class LogoutMenuAction implements MenuAction {
     @Override
     public Object run(Pane mainPane) throws Exception {
-        System.exit(0);
+        //System.exit(0);
+        //Utils.goForward(new Utils.ViewDef("/login.fxml"), );
+
+        LoginModel loginModel = new LoginModel();
+        LoginPresenter loginPresenter = new LoginPresenter(loginModel, new LoginView());
+
+        ((VBox)(mainPane.getParent())).getChildren().remove(0);
+
+        Utils.goForward(new Utils.ViewDef("/login.fxml", loginPresenter), mainPane);
+
         return null;
     }
 }
