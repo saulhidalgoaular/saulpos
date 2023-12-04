bb39fb73d127d732812a60a8f1bd392258d5cf4e
Saul Hidalgo <saulhidalgoaular@gmail.com>
2023-12-04 14:47:57 +0300

    Adding license

--- src/main/java/com/saulpos/model/dao/DatabaseConnection.java
+++ src/main/java/com/saulpos/model/dao/DatabaseConnection.java
@@ -1,14 +1,24 @@
+/*
+ * Copyright (C) 2012-2023 Saúl Hidalgo <saulhidalgoaular at gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.saulpos.model.dao;
 
 import com.saulpos.javafxcrudgenerator.model.Function;
 import com.saulpos.javafxcrudgenerator.model.dao.AbstractBean;
 import com.saulpos.javafxcrudgenerator.model.dao.AbstractBeanImplementationSoftDelete;
 import com.saulpos.javafxcrudgenerator.model.dao.AbstractDataProvider;
-import com.saulpos.model.bean.MenuModel;
-import com.saulpos.model.bean.Permission;
-import com.saulpos.model.bean.Profile;
-import com.saulpos.model.bean.UserB;
-import com.saulpos.model.menu.DefaultMenuGenerator;
 import jakarta.persistence.*;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
