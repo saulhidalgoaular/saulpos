05ed68c841175d2b12b955b31e376e8bfb11f84d
Saul Hidalgo <saulhidalgoaular@gmail.com>
2024-01-06 03:42:49 +0300

    POS Basic structure

--- src/main/java/com/saulpos/presenter/POSMainPresenter.java
+++ src/main/java/com/saulpos/presenter/POSMainPresenter.java
@@ -2,10 +2,14 @@ package com.saulpos.presenter;
 
 import com.saulpos.model.POSMainModel;
 import com.saulpos.view.POSMainView;
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
 
 public class POSMainPresenter extends AbstractPresenter<POSMainModel, POSMainView> {
 
     // Create the FXML components to bind
+    @FXML
+    public VBox mainPOSVBox;
 
     public POSMainPresenter(POSMainModel model, POSMainView view) {
         super(model, view);
