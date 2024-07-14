0c1f597f68acc661efea7b8c01425a1868bf14cc
Tareq Sefati <tareq.sefati@gmail.com>
2024-07-15 03:09:18 +0600

    Implement crud view for vat and check for empty or invalid vat addition.

--- src/main/java/com/saulpos/model/menu/action/ManageProductsMenuAction.java
+++ src/main/java/com/saulpos/model/menu/action/ManageProductsMenuAction.java
@@ -27,6 +27,7 @@ import com.saulpos.model.MainModel;
 import com.saulpos.model.bean.Discount;
 import com.saulpos.model.bean.Price;
 import com.saulpos.model.bean.Product;
+import com.saulpos.model.bean.Vat;
 import com.saulpos.model.dao.DatabaseConnection;
 import com.saulpos.model.dao.HibernateDataProvider;
 import com.saulpos.view.AbstractView;
@@ -42,8 +43,9 @@ import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -156,29 +158,29 @@ public class ManageProductsMenuAction extends CrudMenuAction{
                 CrudPresenter crud = crudGenerator.generate();
 
                 ((Button)crud.getView().getSaveButton()).setOnAction(
-                        new EventHandler<ActionEvent>() {
-                            @Override
-                            public void handle(ActionEvent actionEvent) {
-                                try {
-                                    crud.getModel().saveItemAction();
-                                    // Let's select the item in case it was a new one.
+                    new EventHandler<ActionEvent>() {
+                        @Override
+                        public void handle(ActionEvent actionEvent) {
+                            try {
+                                crud.getModel().saveItemAction();
+                                // Let's select the item in case it was a new one.
 
-                                    if (crud.getView().getTableView().getSelectionModel().selectedItemProperty().get() == null){
-                                        crud.getView().getTableView().getSelectionModel().selectLast();
-                                    }
-
-
-                                    // FIXME We are querying the database twice, it is not ideal, but let's leave it like this for now
-
-                                    List allItems = dataProvider.getAllItems(Product.class, product, AbstractDataProvider.SearchType.EQUAL);
-                                    Set<Price> set = ((Product) allItems.get(0)).getPriceList();
-                                    product.setPriceList(set);
-                                    product.saveOrUpdate();
-                                } catch (Exception e) {
-                                    DialogBuilder.createExceptionDialog("Exception saving the item", "SAUL POS", e.getMessage(), e).showAndWait();
+                                if (crud.getView().getTableView().getSelectionModel().selectedItemProperty().get() == null){
+                                    crud.getView().getTableView().getSelectionModel().selectLast();
                                 }
+
+
+                                // FIXME We are querying the database twice, it is not ideal, but let's leave it like this for now
+
+                                List allItems = dataProvider.getAllItems(Product.class, product, AbstractDataProvider.SearchType.EQUAL);
+                                Set<Price> set = ((Product) allItems.get(0)).getPriceList();
+                                product.setPriceList(set);
+                                product.saveOrUpdate();
+                            } catch (Exception e) {
+                                DialogBuilder.createExceptionDialog("Exception saving the item", "SAUL POS", e.getMessage(), e).showAndWait();
                             }
                         }
+                    }
                 );
 
 
@@ -271,18 +273,25 @@ public class ManageProductsMenuAction extends CrudMenuAction{
                             @Override
                             public void handle(ActionEvent actionEvent) {
                                 try{
-                                    discountCrudPresenter.getModel().saveItemAction();
-                                    if(discountCrudPresenter.getView().getTableView().getSelectionModel().selectedItemProperty().get() == null){
-                                        discountCrudPresenter.getView().getTableView().getSelectionModel().selectLast();
-                                    }
-                                    if(product.getDiscount() == null){
-                                        //create new discount for this product
-                                        product.setDiscount((Discount) dataProvider.getAllItems(Discount.class).getLast());
-                                        product.saveOrUpdate();
+                                    if(discountCrudPresenter.getView().getTableView().getItems().size() > 0 &&
+                                            discountCrudPresenter.getView().getTableView().getSelectionModel().getSelectedItem() ==null){
+                                        DialogBuilder.createError("Error", "SAUL POS",
+                                                "Adding multiple discount information for a single product is not allowed!").showAndWait();
                                     }else{
-                                        //Discount entity already updated. Update the product
-                                        product.getDiscount().setLastModificationTime(LocalDateTime.now());
-                                        product.saveOrUpdate();
+                                        //Condition for not to add the empty discount details
+                                        if(discountCrudPresenter.getModel().getBeanInEdition().getDescription() != null
+                                                && discountCrudPresenter.getModel().getBeanInEdition().getPercentage() > 0
+                                                && discountCrudPresenter.getModel().getBeanInEdition().getStartingDate() != null
+                                                && discountCrudPresenter.getModel().getBeanInEdition().getEndingDate() != null
+                                                && discountCrudPresenter.getModel().getBeanInEdition().getStartingDate()
+                                                    .isBefore(discountCrudPresenter.getModel().getBeanInEdition().getEndingDate()
+                                                )){
+                                            discountCrudPresenter.getModel().saveItemAction();
+                                            product.setDiscount((Discount) dataProvider.getAllItems(Discount.class).getLast());
+                                            product.saveOrUpdate();
+                                            discountCrudPresenter.getModel().refreshAction();
+                                            discountCrudPresenter.getView().getTableView().getSelectionModel().selectLast();
+                                        }
                                     }
                                 } catch (Exception e) {
                                     DialogBuilder.createExceptionDialog("Exception saving the item", "SAUL POS", e.getMessage(), e).showAndWait();
@@ -297,8 +306,123 @@ public class ManageProductsMenuAction extends CrudMenuAction{
             }
         };
 
+        NodeConstructor vatButtonConstructor = new NodeConstructor() {
+            @Override
+            public Node generateNode(Object... objects) {
+                Button vatBtn = new Button();
+                Label icon = GlyphsDude.createIconLabel(FontAwesomeIcon.PERCENT, crudGeneratorParameter.translate("Vat"), "20px", "10px", ContentDisplay.LEFT);
+                vatBtn.setGraphic(icon);
+                vatBtn.setPrefWidth(crudGeneratorParameter.getButtonWidth());
+                return vatBtn;
+            }
+        };
+        Function vatButtonFunction = new Function() {
+            @Override
+            public Object[] run(Object[] objects) throws Exception {
+                Product product = (Product) objects[0];
+                TextFlow textFlow = new TextFlow();
+                Text text1=new Text("Assign the vat for the product: ");
+                Text text2=new Text(product.getDescription());
+                text2.setStyle("-fx-font-weight: bold");
+                textFlow.getChildren().addAll(text1, text2);
+
+                VBox vBox = new VBox(5);
+                vBox.setPadding(new Insets(30));
+
+                CrudGeneratorParameter<Vat> crudVatParamGenerator = new CrudGeneratorParameter<>();
+                crudVatParamGenerator.setClazz(Vat.class);
+
+                HibernateDataProvider vatDataProvider = new HibernateDataProvider(){
+                    @Override
+                    public List getAllItems(Class aClass, AbstractBean abstractBean, SearchType type) {
+                        Vat vat = new Vat();
+                        vat.setProduct(product);
+                        try {
+                            List list = DatabaseConnection.getInstance().listBySample(Vat.class, vat, SearchType.EQUAL);
+                            return list;
+                        } catch (Exception e) {
+                            DialogBuilder.createExceptionDialog("Error", "Error query the database", e.getMessage(), e).showAndWait();
+                        }
+                        return new ArrayList<>();
+                    }
+                };
+
+                NodeConstructor vatBackButtonConstructor = new NodeConstructor() {
+                    @Override
+                    public Node generateNode(Object... objects) {
+                        Button customVatBackButton = new Button();
+                        Label icon = GlyphsDude.createIconLabel(FontAwesomeIcon.BACKWARD, crudVatParamGenerator.translate("Back"), "20px", "10px", ContentDisplay.LEFT);
+                        customVatBackButton.setGraphic(icon);
+                        customVatBackButton.setPrefWidth(crudVatParamGenerator.getButtonWidth());
+                        return customVatBackButton;
+                    }
+                };
+                Function vatBackButtonFunction = new Function() {
+                    @Override
+                    public Object[] run(Object[] objects) throws Exception {
+                        if(viewDef != null){
+                            Utils.goBack(viewDef, mainPane);
+                        }
+                        return null;
+                    }
+                };
+                crudVatParamGenerator.addCustomButton(new CustomButton(vatBackButtonConstructor, vatBackButtonFunction, false));
+                crudVatParamGenerator.setDataProvider(vatDataProvider);
+
+                CrudGenerator<Vat> crudVatGenerator = new CrudGenerator<>(crudVatParamGenerator){
+                    @Override
+                    public CrudPresenter<Vat> generate() throws Exception {
+                        final CrudModel<Vat> model = new CrudModel<>(crudVatParamGenerator){
+                            @Override
+                            public Vat getNewBean() {
+                                Vat newBean = new Vat();
+                                product.setVat(newBean);
+                                return newBean;
+                            }
+                        };
+                        final CrudView view = new CrudViewGenerator(crudVatParamGenerator).generate();
+                        return new CrudPresenter<Vat>(model, view);
+                    }
+                };
+                CrudPresenter<Vat> vatCrudPresenter = crudVatGenerator.generate();
+
+                ((Button) vatCrudPresenter.getView().getSaveButton()).setOnAction(
+                        new EventHandler<ActionEvent>() {
+                            @Override
+                            public void handle(ActionEvent actionEvent) {
+                                try{
+                                    if(vatCrudPresenter.getView().getTableView().getItems().size() > 0 &&
+                                        vatCrudPresenter.getView().getTableView().getSelectionModel().getSelectedItem() ==null){
+                                        DialogBuilder.createError("Error", "SAUL POS",
+                                                "Adding multiple vat information for a single product is not allowed!").showAndWait();
+                                    }else{
+                                        //Condition for not to add the empty vat details
+                                        if(vatCrudPresenter.getModel().getBeanInEdition().getDescription() != null
+                                                && vatCrudPresenter.getModel().getBeanInEdition().getPercentage() > 0){
+                                            vatCrudPresenter.getModel().saveItemAction();
+                                            product.setVat((Vat) dataProvider.getAllItems(Vat.class).getLast());
+                                            product.saveOrUpdate();
+                                            vatCrudPresenter.getModel().refreshAction();
+                                            vatCrudPresenter.getView().getTableView().getSelectionModel().selectLast();
+                                        }
+                                    }
+                                } catch (Exception e) {
+                                    DialogBuilder.createExceptionDialog("Exception saving the item", "SAUL POS", e.getMessage(), e).showAndWait();
+                                }
+                            }
+                        }
+                );
+
+                AbstractView view = new AbstractView(vatCrudPresenter.getView().getMainView());
+                vBox.getChildren().addAll(textFlow, view.getRoot());
+                Utils.goForward(new AbstractView(vBox), mainPane);
+                return null;
+            }
+        };
+
         crudGeneratorParameter.addCustomButton(new CustomButton(customButtonConstructor, customButtonFunction, true));
         crudGeneratorParameter.addCustomButton(new CustomButton(discountButtonConstructor, discountButtonFunction, true));
+        crudGeneratorParameter.addCustomButton(new CustomButton(vatButtonConstructor, vatButtonFunction, true));
         crudGeneratorParameter.setDataProvider(dataProvider);
         CrudGenerator crudGenerator = new CrudGenerator<>(crudGeneratorParameter);
         CrudPresenter crud = crudGenerator.generate();
